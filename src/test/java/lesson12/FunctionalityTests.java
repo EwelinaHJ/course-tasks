51218a9d8a8936fae6b2722f9b1b437270de52f4
Ewelina Halec-Jedras <ehalec1991@gmail.com>
2025-01-23 11:41:03 +0100

    Testy - filtrowanie, sortowanie produktów

--- src/test/java/lesson12/FunctionalityTests.java
+++ src/test/java/lesson12/FunctionalityTests.java
@@ -51,160 +51,157 @@ public class FunctionalityTests {
     public void filteringByPrice() {
 
         ChromeDriver driver = new ChromeDriver();
-        Duration timeToWait = Duration.ofSeconds(25);
-        driver.manage().timeouts().implicitlyWait(timeToWait);
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+
         driver.get("https://demo.prestashop.com/#/en/front");
 
-        By iframe = By.id("framelive");
-        WebElement iframeObject = driver.findElement(iframe);
+        WebElement iframeObject = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("framelive")));
         driver.switchTo().frame(iframeObject);
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         By allProductsLocator = By.cssSelector(".all-product-link");
         WebElement allProductButton = wait.until(ExpectedConditions.elementToBeClickable(allProductsLocator));
         allProductButton.click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".faceted-slider")));
 
         Actions actions = new Actions(driver);
-        while (!driver.findElement(By.cssSelector(".faceted-slider p")).getText().equals("€16.00 - €44.00")) {
+        String sliderValue;
+        do {
             WebElement handle = driver.findElement(By.cssSelector(".ui-slider-handle:nth-of-type(1)"));
-            actions.moveToElement(handle).clickAndHold().moveByOffset(5, 0).perform();
-        }
+            actions.moveToElement(handle)
+                    .clickAndHold()
+                    .moveByOffset(5, 0)
+                    .release()
+                    .perform();
+            sliderValue = driver.findElement(By.cssSelector(".faceted-slider p")).getText();
+        } while (!sliderValue.equals("€16.00 - €44.00"));
+
+
+        By spinnerLocator = By.cssSelector(".spinner");
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(spinnerLocator));
 
         By productContainer = By.cssSelector(".product");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(productContainer));
+        wait.until(ExpectedConditions.numberOfElementsToBe(productContainer, 10));
 
-        List<WebElement> allProductContainer = driver.findElements(productContainer);
-
-        List<WebElement> productsInOrder = new ArrayList<>();
-
-        for (int i = 1; i <= allProductContainer.size(); i++) {
-            String locator = String.format(".product:nth-of-type(%s)", i);
-            WebElement product = driver.findElement(By.cssSelector(locator));
-            productsInOrder.add(product);
-        }
+        List<WebElement> allProducts = driver.findElements(productContainer);
 
         List<String> pricesInOrder = new ArrayList<>();
-        for (WebElement product : productsInOrder) {
+        for (WebElement product : allProducts) {
             String price = product.findElement(By.cssSelector(".price")).getText();
-            price = price.replace("€", "").trim();
-            pricesInOrder.add(price);
+            pricesInOrder.add(price.replace("€", "").trim());
         }
 
-        List <String> expectedPrices = List.of("22.94","22.94","22.94","34.46","34.80","34.80","34.80","22.68","22.68","22.68","42.00","16.68");
+        Assertions.assertEquals(10, allProducts.size(), "Nieprawidłowa liczba produktów po filtrowaniu");
 
+        System.out.println("Znalezione ceny: " + pricesInOrder);
 
-        System.out.println("Ceny rzeczywiste: " + pricesInOrder);
-        System.out.println("Ceny oczekiwane: " + expectedPrices);
-//       Assertions.assertEquals(pricesInOrder,expectedPrices);
-
+        driver.quit();
     }
 
     @Test
-
     public void filterReset() {
-
-        ChromeDriver driver = new ChromeDriver();
-        Duration timeToWait = Duration.ofSeconds(25);
-        driver.manage().timeouts().implicitlyWait(timeToWait);
-        driver.get("https://demo.prestashop.com/#/en/front");
-        By iframe = By.id("framelive");
-        WebElement iframeObject = driver.findElement(iframe);
-        driver.switchTo().frame(iframeObject);
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-        By allProductsLocator = By.cssSelector(".all-product-link");
-        WebElement allProductButton = wait.until(ExpectedConditions.elementToBeClickable(allProductsLocator));
-        allProductButton.click();
-
-        By categoryLocator = By.xpath("//label[@class='facet-label']//a[contains(text(), \"Accessories\")]");
-        wait.until(ExpectedConditions.elementToBeClickable(categoryLocator));
-        WebElement categoryChoiceButton = driver.findElement(categoryLocator);
-        categoryChoiceButton.click();
-
-        By productFilterBox = By.cssSelector(".product");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(productFilterBox));
-
-        List<WebElement> allProductContainer = driver.findElements(productFilterBox);
-
-        List<WebElement> productFiltered = new ArrayList<>();
-
-        for (int i = 1; i <= allProductContainer.size(); i++) {
-            String locator1 = String.format(".product:nth-of-type(%s)", i);
-            WebElement product = driver.findElement(By.cssSelector(locator1));
-            productFiltered.add(product);
-        }
-        int productCountBeforeReset = allProductContainer.size();
-        System.out.println("Liczba produktów z filtrem: " + productCountBeforeReset);
-
-
-        By resetLocator = By.cssSelector("#_desktop_search_filters_clear_all button");
-        WebElement resetButton = wait.until(ExpectedConditions.elementToBeClickable(resetLocator));
-        resetButton.click();
-
-        By productNoFilterBox = By.cssSelector(".product");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(productNoFilterBox));
-
-        List<WebElement> allProductContainer1 = driver.findElements(productNoFilterBox);
-
-        List<WebElement> productNoFiltered = new ArrayList<>();
-        for (int i = 1; i <= allProductContainer1.size(); i++) {
-            String locator2 = String.format(".product:nth-of-type(%s)", i);
-            WebElement product1 = driver.findElement(By.cssSelector(locator2));
-            productNoFiltered.add(product1);
-        }
-        int productCountAfterReset = allProductContainer1.size();
-        System.out.println("Liczba produktów po resecie filtrów: " + productCountAfterReset);
-
-    }
-
-    @Test
-    public void visibilityOfFilters() {
-
         ChromeDriver driver = new ChromeDriver();
         Duration timeToWait = Duration.ofSeconds(30);
         driver.manage().timeouts().implicitlyWait(timeToWait);
         driver.get("https://demo.prestashop.com/#/en/front");
-        By iframe = By.id("framelive");
-        WebElement iframeObject = driver.findElement(iframe);
-        driver.switchTo().frame(iframeObject);
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(40));
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.id("framelive")));
+
         By allProductsLocator = By.cssSelector(".all-product-link");
-        WebElement allProductButton = wait.until(ExpectedConditions.elementToBeClickable(allProductsLocator));
-        allProductButton.click();
+        wait.until(ExpectedConditions.elementToBeClickable(allProductsLocator)).click();
 
-        By availableLocator = By.xpath("//label[@class='facet-label']//a[contains(text(), \"Available\")]");
-        wait.until(ExpectedConditions.elementToBeClickable(availableLocator));
-        WebElement availableButton = driver.findElement(availableLocator);
-        availableButton.click();
+        By totalProductsCounter = By.cssSelector(".total-products");
+        String totalProductsText = wait.until(ExpectedConditions.visibilityOfElementLocated(totalProductsCounter)).getText();
+        int expectedTotalProducts = Integer.parseInt(totalProductsText.replaceAll("[^0-9]", ""));
+        System.out.println("Oczekiwana całkowita liczba produktów (z licznika): " + expectedTotalProducts);
 
-        Assertions.assertTrue(availableButton.isDisplayed());
-
-        By paperTypeLocator = By.xpath("//label[@class='facet-label']//a[contains(text(), 'Ruled')]");
-        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(paperTypeLocator));
-        WebElement ruledButton = driver.findElement(availableLocator);
-        ruledButton.click();
+        By productBox = By.cssSelector(".product");
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(productBox));
+        int page1Count = driver.findElements(productBox).size();
+        System.out.println("Liczba produktów na stronie 1: " + page1Count);
 
 
+        By nextPageButton = By.cssSelector("li a[rel='next']");
+        wait.until(ExpectedConditions.elementToBeClickable(nextPageButton)).click();
+
+        By currentPage = By.cssSelector(".page-list .current");
+        wait.until(ExpectedConditions.textToBe(currentPage, "2"));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(productBox));
+
+        int page2Count = driver.findElements(productBox).size();
+        System.out.println("Liczba produktów na stronie 2: " + page2Count);
+
+        int totalInitialCount = page1Count + page2Count;
+        System.out.println("Całkowita początkowa liczba produktów: " + totalInitialCount);
+
+        Assertions.assertEquals(expectedTotalProducts, totalInitialCount,
+                "Suma produktów z obu stron powinna być zgodna z licznikiem");
+
+        By prevPageButton = By.cssSelector("li a[rel='prev']");
+        wait.until(ExpectedConditions.elementToBeClickable(prevPageButton)).click();
+        wait.until(ExpectedConditions.textToBe(currentPage, "1"));
+
+        By categoryLocator = By.xpath("//label[@class='facet-label']//a[contains(text(), 'Accessories')]");
+        wait.until(ExpectedConditions.elementToBeClickable(categoryLocator)).click();
+
+        wait.until(ExpectedConditions.not(ExpectedConditions.textToBe(totalProductsCounter, totalProductsText)));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(productBox));
+        int filteredProductCount = driver.findElements(productBox).size();
+        System.out.println("Liczba produktów po filtrowaniu: " + filteredProductCount);
+
+        By resetLocator = By.cssSelector("#_desktop_search_filters_clear_all button");
+        wait.until(ExpectedConditions.elementToBeClickable(resetLocator)).click();
+
+        wait.until(ExpectedConditions.textToBe(totalProductsCounter, totalProductsText));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(productBox));
+        int firstPageAfterReset = driver.findElements(productBox).size();
+
+        wait.until(ExpectedConditions.elementToBeClickable(nextPageButton)).click();
+        wait.until(ExpectedConditions.textToBe(currentPage, "2"));
+        wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(productBox));
+        int secondPageAfterReset = driver.findElements(productBox).size();
+
+        int totalAfterReset = firstPageAfterReset + secondPageAfterReset;
+        System.out.println("Całkowita liczba produktów po resecie: " + totalAfterReset);
+
+        Assertions.assertTrue(filteredProductCount < expectedTotalProducts,
+                "Liczba produktów po filtrowaniu powinna być mniejsza niż początkowa");
+        Assertions.assertEquals(expectedTotalProducts, totalAfterReset,
+                "Liczba produktów po resecie powinna być równa liczbie z licznika");
+
+        driver.quit();
+    }
+
+    @Test
+    public void checkTwoFiltersVisibility() {
+        ChromeDriver driver = new ChromeDriver();
+        Duration timeToWait = Duration.ofSeconds(25);
+        driver.manage().timeouts().implicitlyWait(timeToWait);
+        driver.get("https://demo.prestashop.com/#/en/front");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.id("framelive")));
+
+        By allProductsLocator = By.cssSelector(".all-product-link");
+        wait.until(ExpectedConditions.elementToBeClickable(allProductsLocator)).click();
+
+        By accessoriesLocator = By.xpath("//label[@class='facet-label']//a[contains(text(), 'Accessories')]");
+        wait.until(ExpectedConditions.elementToBeClickable(accessoriesLocator)).click();
+
+
+        By activeFiltersLocator = By.cssSelector(".active-filter-title");
+        boolean isAccessoriesFilterVisible = wait.until(ExpectedConditions.visibilityOfElementLocated(activeFiltersLocator)).isDisplayed();
+        System.out.println("Czy filtr Accessories jest widoczny: " + isAccessoriesFilterVisible);
+
+        By ruledLocator = By.xpath("//label[@class='facet-label']//a[contains(text(), 'Ruled')]");
+        wait.until(ExpectedConditions.elementToBeClickable(ruledLocator)).click();
+
+        By activeFiltersLocator2 = By.cssSelector(".active-filter-title");
+        boolean isRuledFilterVisible = wait.until(ExpectedConditions.visibilityOfElementLocated(activeFiltersLocator2)).isDisplayed();
+        System.out.println("Czy filtr Accessories jest widoczny: " + isRuledFilterVisible);
+
+        driver.quit();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
