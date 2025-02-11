71369a93407ccc669abe23b0aced3f5e7366d0e1
Ewelina Halec-Jedras <ehalec1991@gmail.com>
2025-02-11 19:03:22 +0100

    Zadania klasy abstrakcyjne i interface

---
+++ src/test/java/abstractclass/homework/Character.java
@@ -0,0 +1,24 @@
+package abstractclass.homework;
+
+public abstract class Character implements Creature{
+
+    protected int health;
+    protected int strength;
+
+    public Character(int health, int strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    public abstract void takeDamage();
+
+
+    public abstract void heal();
+
+
+
+    @Override
+    public int getHealth() {
+        return health;
+    }
+}
---
+++ src/test/java/abstractclass/homework/Dragon.java
@@ -0,0 +1,38 @@
+package abstractclass.homework;
+
+public class Dragon extends Character implements Flying {
+
+    public Dragon(int health, int strength) {
+        super(health, strength);
+    }
+
+    @Override
+    public void takeDamage() {
+
+    }
+
+    @Override
+    public void heal() {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void defend() {
+
+    }
+
+    @Override
+    public void fly() {
+
+    }
+}
